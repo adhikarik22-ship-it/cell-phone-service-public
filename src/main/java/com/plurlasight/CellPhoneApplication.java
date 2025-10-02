c7d4fc5dcd49e7bce80736b0def97b837aab747c
khagendraadhikari <kadhikari@my.yearupunited.org>
2025-10-02 09:36:10 -0500

    created instance

--- src/main/java/com/plurlasight/CellPhoneApplication.java
+++ src/main/java/com/plurlasight/CellPhoneApplication.java
@@ -1,10 +1,34 @@
 package com.plurlasight;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
 
 public class CellPhoneApplication {
 
     public static void main(String[] args) {
+        // instance of cellphone
+        CellPhone myCellPhone = new CellPhone();
+        Scanner scanner = new Scanner (System.in);
 
-        
+        //phone info prompt.
+        System.out.println("Serial Number: ");
+        String serialNumber = scanner.nextLine();
+
+        System.out.println("Model: ");
+        String model = scanner.nextLine();
+
+        System.out.println("Carrier: ");
+        String carrier = scanner.nextLine();
+
+        System.out.println("Phone Number");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Owner's Name:");
+        String owner = scanner.nextLine();
+
+
+
+
+
 
 
 
